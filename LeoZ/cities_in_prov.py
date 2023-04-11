1e52d97b37396f12c7b6806253b58011549556fb
Leo Zhao <zhao0213@agongquinlive.com>
2023-04-10 21:00:44 -0400

    updated

--- LeoZ/cities_in_prov.py
+++ LeoZ/cities_in_prov.py
@@ -1,12 +1,14 @@
 ﻿import sys
 
+
 prov_codes = ['BC', 'AB', 'SK', 'MB', 'ON', 'QC', 'NB', 
               'PE', 'NS', 'NL', 'YT', 'NT', 'NU']
-                    
-    
-def main():
 
-    prov = sys.argv[1].upper()
+def main():
+    fc   = sys.argv[1]
+    # in_field = sys.argv[2]
+    # out_ws  = sys.argv[3]
+    prov = sys.argv[2].upper()
     if prov not in prov_codes:
         print (f'Invalid prov.  Must be one of {", ".join(prov_codes)}')
         exit()
@@ -17,7 +19,7 @@ def main():
     arcpy.env.workspace = ws
     prov_field = arcpy.AddFieldDelimiters(ws, 'Prov')
     wc =  f"{prov_field}='{prov}'"
-    fc = 'MajorCities'
+  
     arcpy.AddMessage('Name,Prov,Longitude,Latitude')
     with arcpy.da.SearchCursor(fc, 
                                ['PROV','Name', 'SHAPE@XY'], 
